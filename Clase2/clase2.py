5dccd97dad8832cea4e9eec0ad806542e4b7ce84
Carlos Granados <josuegrana2s@gmail.com>
2026-07-27 11:51:32 -0600

    adding class2

---
+++ Clase2/clase2.py
@@ -0,0 +1,18 @@
+#Tipos De Datos
+x = 10 #int/ numero sin decimales
+y = 3.14 #float numero con decimales
+print ("x*y") #
+print (x*y)
+
+#cadenas (srt) - texto
+name = " Carlos " #STR
+print (name)
+
+#Bool true - false
+valido = True
+print (valido)
+
+lista = [1,2,3]
+print(id(lista))
+lista.append(4)
+print(id(lista)) #misma referencia
