924acbaad33607d690da95e1bebb36932be68ffa
Carlos Granados <josuegrana2s@gmail.com>
2026-07-27 11:51:41 -0600

    adding class 4

---
+++ Clase4/temp.py
@@ -0,0 +1,29 @@
+import sys
+#print ("-------- Numeros Enteros --------")
+#print (f"Tamaño de un entero: {sys.getsizeof(0)} bytes")
+#numero = 10**100
+#print(numero)
+#TIPO DE DATO PY
+
+#print("-------- Numero Decimales (float) --------")
+#print(f"tamaño maximo de un decimal: {sys.float_info.max}")
+#print(f"tamaño minimo de un decimal: {sys.float_info.min}")
+#print(1.25**100)
+
+#print ("-------- Valor Booleano --------")
+#print(True)
+#print(False)
+#print(type(False))
+
+#print(int(True))
+#print(int(False))
+
+#cadena = "Hola Mundo"
+#print(cadena)
+#print ("Longitud de la palabra:", len(cadena))
+
+#PRINT PY
+#print("Hola mi nombre es \"Carlos \" ")
+#print ("Hola mi nombre es Carlos\nSoy estudiante de la materia de\nEstructura de datos ")
+#print ("nombre | nota1 | nota2 | nota3\nAna    |   10  |   10  |   10  \nJosue  |   9   |   8   |   7   |")
+print("Nombre\tNota1\tNota2\tNota3\nAna\t10\t10\t10\nJosue\t9\t8\t7")
